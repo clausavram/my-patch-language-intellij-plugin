e6d776ac83fa9e39e83d89c65040d4c7c8a2459d
GitHub Action <action@github.com>
2021-04-08 20:17:12 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/clausavram/mypatchlanguageintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.clausavram.mypatchlanguageintellijplugin.services
+
+import com.github.clausavram.mypatchlanguageintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
